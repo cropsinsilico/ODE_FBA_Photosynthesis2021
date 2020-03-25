c01d780b4ced70b79e3fc2cdf4958e999906f072
Sanu Shameer <sanushameer@gmail.com>
2020-03-25 15:48:49 +0000

    Updated change to code and executed it to generate dummy output

--- ePhotosynthesis/Sim_Ephotosynthesis.m
+++ ePhotosynthesis/Sim_Ephotosynthesis.m
@@ -9,6 +9,10 @@ ATPCost=ATPCostTable.data;
 ResultRate=trDynaPS_Drive(Cai,Lii,ATPCost,SucPath,1, 1);
 Output(1)="Light intensity,Vc,Vo,VPGA,VT3P,Vstarch,Vt_glycerate,Vt_glycolate";
 Output(2)=Lii+","+ResultRate(1)+","+ResultRate(2)+","+ResultRate(3)+","+ResultRate(4)+","+ResultRate(5)+","+ResultRate(6)+","+ResultRate(7);
-dlmwrite('OutputRate.txt',Output)
+outfile = fopen("OutputRate.txt","w");
+fprintf(outfile,Output(1));
+fprintf(outfile,"\n");
+fprintf(outfile,Output(2));
+fclose(outfile);
 end
 
