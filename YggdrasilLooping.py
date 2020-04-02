5d768902b67cf541225ea67b3801500b41cc7d8a
Sanu Shameer <sanushameer@gmail.com>
2020-04-02 11:11:21 +0100

    removed unused prog variable

--- YggdrasilLooping.py
+++ YggdrasilLooping.py
@@ -4,4 +4,4 @@ import argparse
 parser = argparse.ArgumentParser(description='Run an integration.')
 parser.add_argument('yamlfile', nargs='+',help='One or more yaml specification files.')
 args = parser.parse_args(["yggdrasil_ODE_FBA_testing.yaml"])
-runner.run(args.yamlfile, ygg_debug_prefix=prog)
+runner.run(args.yamlfile)
