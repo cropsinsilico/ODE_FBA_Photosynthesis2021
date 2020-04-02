77263b64b30ca57bfc3394676ddbb28e4b22bdc0
Sanu Shameer <sanushameer@gmail.com>
2020-04-02 11:03:48 +0100

    Trying to involve yggrun from python

---
+++ YggdrasilLooping.py
@@ -0,0 +1,6 @@
+from yggdrasil import runner
+import argparse
+
+parser = argparse.ArgumentParser(description='Run an integration.')
+parser.add_argument('yamlfile', nargs='+',help='One or more yaml specification files.')
+args = parser.parse_args(["yggdrasil_ODE_FBA_testing.yaml"])
