d917d712725d7e200107d98669cf297cf0df5002
Sanu Shameer <sanushameer@gmail.com>
2020-04-02 11:12:40 +0100

    testing filename corrected

--- YggdrasilLooping.py
+++ YggdrasilLooping.py
@@ -3,5 +3,5 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Run an integration.')
 parser.add_argument('yamlfile', nargs='+',help='One or more yaml specification files.')
-args = parser.parse_args(["yggdrasil_ODE_FBA_testing.yaml"])
+args = parser.parse_args(["yggrasil_ODE_FBA_testing.yaml"])
 runner.run(args.yamlfile)
