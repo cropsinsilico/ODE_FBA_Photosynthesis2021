8dced3dc009331892c4d08c2793e09dbe6f422dc
Sanu Shameer <sanushameer@gmail.com>
2020-07-03 18:10:14 +0100

    fixed minor bug

--- FBA/dielFBA_model_forODE.py
+++ FBA/dielFBA_model_forODE.py
@@ -110,6 +110,6 @@ print("Phloem export rate ="+str(sol.fluxes["diel_biomass"]*4))
 
 
 fout= open("./Daytime_flux.csv","w")
-for rxn in temp.reactions:
+for rxn in leaf_model.reactions:
     fout.write(rxn.id+","+rxn.reaction+","+str(sol.fluxes[rxn.id])+"\n")
 fout.close()
