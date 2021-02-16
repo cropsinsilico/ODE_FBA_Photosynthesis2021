8d2510ac3cac55ff3266df5f8be476554efbe332
sshameer <sanushameer@gmail.com>
2021-02-16 10:20:29 +0530

    Added ODE only for Figure 2B

---
+++ Validations/ODE_Figure2B.py
@@ -0,0 +1,38 @@
+from yggdrasil import runner
+import argparse
+
+parser = argparse.ArgumentParser(description='Run an integration.')
+parser.add_argument('yamlfile', nargs='+',help='One or more yaml specification files.')
+args1 = parser.parse_args(["../ePhotosynthesis/EphotosynthesisOnly.yml"])
+
+
+PPFD = [50,100,200,300,400,500,600,800,1000,1200,1500,2000]
+Vc = list()
+Vo = list()
+Vpga = list()
+Vt3p = list()
+Vstarch = list()
+Vglycerate = list()
+Vglycolate = list()
+
+j=0
+for ci in CO2:
+    j=j+1
+
+    #ensure additional chloroplastic ATP consumption rate (J_ATPase) starts at 0
+    f1 = open("../ePhotosynthesis/InputATPCost.txt","w")
+    f1.write("ATPCost 0")
+    f1.close()
+
+    F_weather = open("../ePhotosynthesis/InputEvn.txt","w")
+    F_weather.write("CO2 400\nPPFD "+str(PPFD)+"\nSucPath 1"+"\ndaylength 12")
+    F_weather.close()
+
+    f3 = open("../ePhotosynthesis/InputNADPHCost.txt","w")
+    f3.write("NADPHCost 0")
+    f3.close()
+
+    runner.run(args1.yamlfile)
+
+    import os
+    os.rename("./../ePhotosynthesis/OutputFluxT.txt","./../Validations/ODEonly_fluxes_Fig2B_"+str(PPFD)+".csv")
