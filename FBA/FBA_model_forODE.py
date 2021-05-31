8aa379e119906c07a8f70ec6fcd497aab3a61ee9
Sanu <sanu@dps017.plants.ox.ac.uk>
2021-06-01 00:34:20 +0100

    TC for Figure 2A rerun with Nitrate

--- FBA/FBA_model_forODE.py
+++ FBA/FBA_model_forODE.py
@@ -378,7 +378,7 @@ for rxn in temp.metabolites.ATP_p.reactions:
 print("Extra APTase flux ="+str(total))
 
 fout= open("./../ePhotosynthesis/InputATPCost.txt","w")
-fout.write("ATPCost	"+str(round(total,4)))
+fout.write("ATPCost	"+str(round(total,3)))
 fout.close()
 
 total = JNADPHox
@@ -401,7 +401,7 @@ for rxn in temp.metabolites.NADH_p.reactions:
 print("Extra NADPH flux ="+str(total))
 
 fout= open("./../ePhotosynthesis/InputNADPHCost.txt","w")
-fout.write("NADPHCost	"+str(round(total,4)))
+fout.write("NADPHCost	"+str(round(total,3)))
 fout.close()
 
 fout= open("./Daytime_flux.csv","w")
